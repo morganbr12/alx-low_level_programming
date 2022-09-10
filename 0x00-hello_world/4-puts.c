205c96868cb0dee72c2a4a114c6f787c2e443d07
fredrick dankwah <f.morgan221@outlook.com>
2022-09-09 19:03:57 -0700

    new chanhes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+/*
+ * This is to use the puts to print out a result
+ * no use of printf
+ */
 int main(void)
 {
         char outPut[] = "\"Programming is like building a multilingual puzzle";
