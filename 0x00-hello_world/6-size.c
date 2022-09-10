d3350f0d8f7cfcc33e3eadde92859651bc06925d
fredrick dankwah <f.morgan221@outlook.com>
2022-09-10 08:48:08 -0700

    data variables

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,15 +8,17 @@
  */
 int main(void)
 {
-	int i;
-	double d;
-	float f;
 	char c;
+	int i;
+	long int li;
+	long long int lli;
+	float f;
 
+	printf("The size of a char: %lu. \n", (unsigned long)sizeof(c));
 	printf("The size of an int is: %lu. \n", (unsigned long)sizeof(i));
-	printf("The size of a double is: %lu. \n", (unsigned long)sizeof(d));
+	printf("The size of a long int is: %lu. \n", (unsigned long)sizeof(li));
+	printf("The size of a long long int is: %lu. \n", (unsigned long)sizeof(lli));
 	printf("The size of a float is: %lu. \n", (unsigned long)sizeof(f));
-	printf("The size of a char is: %lu. \n", (unsigned long)sizeof(f));
 
 	return (0);
 }
