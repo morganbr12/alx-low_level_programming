a5b7c35cd40e1ecd01c65762fc6b9e71dc61560c
fredrick dankwah <f.morgan221@outlook.com>
2022-09-09 19:19:54 -0700

    new one

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,8 +5,8 @@
  */
 int main(void)
 {
-	char outPut[] = "\"Programming is like building a multilingual puzzle";
-	
+	char outPut[] = "\"Programming is like building a multilingual puzzle\n";
 	puts(outPut);
+
 	return (0);
 }
