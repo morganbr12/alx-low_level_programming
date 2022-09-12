4cf42e1b68c586aa409d057a5d8a7ed2275d9239
fredrick dankwah <f.morgan221@outlook.com>
2022-09-11 20:28:46 -0700

    new

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char lowercase_alphabet;
+
+	for (lowercase_alphabet = 'a'; lowercase_alphabet <= 'z'; ++lowercase_alphabet)
+		putchar(lowercase_alphabet);
+
+	return (0);
+}
